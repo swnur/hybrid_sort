397422518f1e3d3dc66a40a02f7fe0e4f6c1eeb4
Nurbek Shukan <shukannurbek@gmail.com>
2023-11-14 13:29:02 +0100

    Finished with implementing HeapSort() function changed whole header file

--- HeapSort.hpp
+++ HeapSort.hpp
@@ -3,38 +3,38 @@
 
 #include <vector>
 
-void make_heap(std:: vector<int>& arr, int n, int i) {
-   int parent = i;
-   int left_child = 2 * parent + 1;
-   int right_child = 2 * parent + 2;
+void siftDown(std:: vector<int>& arr, int i, int n) {
+   int current = i;
+   int left_child = 2 * current + 1;
+   int right_child = 2 * current + 2;
 
-   if (left_child < n && arr[left_child] > arr[parent]) {
-      parent = left_child;
+   if (left_child < n && arr[left_child] > arr[current]) {
+      current = left_child;
+   }
+   if (right_child < n && arr[right_child] > arr[current]) {
+      current = right_child;
    }
 
-   if (right_child <= n && arr[right_child] > arr[parent]) {
-      parent = right_child;
-   }
-
-   if (parent != i) {
-      std:: swap(arr[i], arr[parent]);
-      make_heap(arr, n, parent);
+   if (current != i) {
+      std:: swap(arr[current], arr[i]);
+      siftDown(arr, current, n);
    }
 }
 
-void buildHeap(std:: vector<int>& arr, int n) {
-   for (int i = (n - 1) / 2; i >= 0; i--) {
-      make_heap(arr, i, n);
+void buildHeap(std:: vector<int>& arr) {
+   int n = arr.size();
+   for (int i = n / 2 - 1; i >= 0; --i) {
+      siftDown(arr, i, n);
    }
 }
 
 void heapSort(std:: vector<int>& arr) {
-   int n = arr.size();
-   buildHeap(arr, n);
-   int heapSize = n;
-   for (int i = 0; i < n - 1; i++)  {
-      std:: swap(arr[0], arr[n - i - 1]);
-      make_heap(arr, i, 0);
+   buildHeap(arr);
+   int heapSize = arr.size(), n = arr.size();
+   for (int i = 0; i < n - 1; i++) {
+      std:: swap(arr[0], arr[n- 1 - i]);
+      heapSize--;
+      siftDown(arr, 0, heapSize);
    }
 }
 
--- main.cpp
+++ main.cpp
@@ -13,18 +13,6 @@ std:: ostream& operator<<(std:: ostream& output, std:: vector<int> arr) {
    return output;
 }
 
-bool is_heap(std:: vector<int>& arr) {
-   for (int i = 0 ; i < arr.size() ; i++) {
-        if (2 * i + 1 < arr.size() && arr[i] < arr[2 * i + 1]) {
-            return false;
-        }
-        if (2 * i + 2 < arr.size() && arr[i] < arr[2 * i + 2]) {
-            return false;
-        }
-    }
-
-    return true;
-}
 
 void runTestCases() {
    std::map<std::vector<int>, std::vector<int>> testCases = {
@@ -38,7 +26,7 @@ void runTestCases() {
       std::vector<int> input = testCase.first;
       std::vector<int> expected = testCase.second;
 
-      quickSort(input, 0, input.size() - 1);
+      heapSort(input);
       std:: cout << input << std:: endl;
       assert(input == expected);
    }
@@ -46,22 +34,9 @@ void runTestCases() {
    std::cout << "All test cases passed!" << std::endl;
 }
 
-void testHeap() {
-   std:: vector<int> test_arr = {1, 7, 3, 4, 2};
-   int n = test_arr.size();
-   for (int i = (n - 1) / 2; i >= 0; i--) {
-      make_heap(test_arr, n, i);
-   }
-
-   std:: cout << test_arr << std:: endl;
-   if (!is_heap(test_arr)) {
-        std::cout << "make_heap does not work" << std::endl;
-   }
-}
-
 int main() {
    
-   testHeap();
+   runTestCases();
 
    return 0;
 }
