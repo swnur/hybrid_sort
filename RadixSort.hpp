bc8eb2a5f4edc8ba1cb265c770b439f69111a46b
Nurbek Shukan <shukannurbek@gmail.com>
2023-11-15 10:34:42 +0100

    Implemented RadixSort using Counting Sort with LSD(Least significant digit) but only for positive numbers

--- RadixSort.hpp
+++ RadixSort.hpp
@@ -2,6 +2,7 @@
 #define RADIXSORT_HPP
 
 #include <vector>
+#include <algorithm>
 
 std:: vector<int> countingSort(std:: vector<int>& arr) {
     int minElement = *std::min_element(arr.begin(), arr.end());
@@ -28,8 +29,34 @@ std:: vector<int> countingSort(std:: vector<int>& arr) {
     return output;
 }
 
-void radixSort(std:: vector<int>& arr) {
+std:: vector<int> countingSortForRadixSort(std:: vector<int>& arr, int exp) {
+    std::vector<int> count(10, 0);
+    for (int num : arr) {
+        count[(num / exp) % 10]++;
+    }
 
+    for (int i = 1; i < 10; i++) {
+        count[i] += count[i - 1];
+    }
+
+    std::vector<int> output(arr.size());
+
+    for (int i = arr.size() - 1; i >= 0; i--) {
+        int current = arr[i];
+        int positionInArray = count[(current / exp) % 10] - 1;
+        output[positionInArray] = current;
+        count[(current / exp) % 10]--;
+    }
+
+    return output;
+}
+
+
+void radixSort(std:: vector<int>& arr) {
+    int maxNum = *std::max_element(arr.begin(), arr.end());
+    for (int exp = 1; maxNum / exp > 0; exp *= 10) {
+        arr = countingSortForRadixSort(arr, exp);
+    }
 }
 
 #endif
--- main.cpp
+++ main.cpp
@@ -18,8 +18,8 @@ std:: ostream& operator<<(std:: ostream& output, std:: vector<int> arr) {
 void runTestCases() {
    std::map<std::vector<int>, std::vector<int>> testCases = {
       {{4, 11, 3, 6, 0, 1, 8, 5}, {0, 1, 3, 4, 5, 6, 8, 11}},
-      {{0, 0, 0, 0, 0, 0, -1}, {-1, 0, 0, 0, 0, 0, 0}},
-      {{1, -3, 0}, {-3, 0, 1}},
+      // {{0, 0, 0, 0, 0, 0, -1}, {-1, 0, 0, 0, 0, 0, 0}},
+      // {{1, -3, 0}, {-3, 0, 1}},
       {{1, 3, 4, 5, 6, 7, 8, 9, 10}, {1, 3, 4, 5, 6, 7, 8, 9, 10}}
     };
 
@@ -30,10 +30,10 @@ void runTestCases() {
       // quickSort(input, 0, input.size() - 1);
       // heapSort(input);
       // insertionSort(input);
-      // radixSort(input);
-      std:: vector<int> result = countingSort(input);
-      std:: cout << result << std:: endl;
-      assert(result == expected);
+      radixSort(input);
+      // std:: vector<int> result = countingSort(input);
+      std:: cout << input << std:: endl;
+      assert(input == expected);
    }
 
    std::cout << "All test cases passed!" << std::endl;
@@ -41,6 +41,16 @@ void runTestCases() {
 
 int main() {
    
+   std::vector<int> arr = {170, 45, 75, 90, 802, 24, 2, 66};
+    
+   std::cout << "Original array: ";
+   std:: cout << arr << std:: endl;
+
+   radixSort(arr);
+
+   std::cout << "Sorted array: ";
+   std::cout << arr << std:: endl;
+
    runTestCases();
 
    return 0;
